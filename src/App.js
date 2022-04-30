48986deee8cd69951df79a55090e5c851cbfd66c
Clément Notin <clem.notin@gmail.com>
2022-04-30 22:39:06 +0200

    first commit to-do-list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from "./components/Header";
+import {FaTrash} from "react-icons/fa";
+import {useState} from "react";
 
 function App() {
+  const [task, setTask] = useState([]);
+  const [value, setValue] = useState();
+  const handleChange = (e) =>{
+    setValue(e.target.value);
+  }
+  const handleClick = (e) =>{
+    e.preventDefault();
+    setTask([...task, value]);
+    console.log(task);
+    var form = document.getElementById("myForm");
+    form.reset();
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <div className='addBar'>
+        <form id="myForm">
+          <input id="inputTask" placeholder= "New Task" onChange={handleChange}></input>
+          <button type="submit" onClick={handleClick}>Add Task</button>
+        </form>
+        <div className={task.length > 0? 'taskAdded' : 'taskAdded hide'}>
+          <input id="inputCheck"  type="checkbox"></input>
+          <span>{value}</span>
+          <FaTrash/>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import {FaTasks} from "react-icons/fa";
+
+const Header = () => {
+  return <div className="header">
+      <FaTasks/>
+      <span>To Do List</span>
+      </div>;
+}
+
+export default Header;
